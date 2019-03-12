97eb8fbbfc22d11678e6365894e59b887c99fa02
Unknown <alexandr.myazin@gmail.com>
2019-03-12 18:01:19 +0200

    homework 2
    indexing refine

--- xor_encryption.py
+++ xor_encryption.py
@@ -1,11 +1,8 @@
 def xor_encrypt(text, key):
     encrypted = ''
-    key_index = 0
-    max_key_index = len(key) - 1
 
-    for ch in text:
-        encrypted += chr(ord(ch) ^ ord(key[key_index]))
-        key_index = key_index + 1 if key_index < max_key_index else 0
+    for idx, ch in enumerate(text):
+        encrypted += chr(ord(ch) ^ ord(key[idx % len(key)]))
 
     return encrypted
 
